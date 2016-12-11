64ea5455911eed11a84d104e6f7f3f0e3c26dc8f
Nathan Chancellor <natechancellor@gmail.com>
2016-12-11 00:44:45 -0700

    tcversion.mk: clean up and generalize
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- configs/tcversion.mk
+++ configs/tcversion.mk
@@ -7,93 +7,68 @@ ifeq (Linux,$(UNAME))
 endif
 
 ifeq (linux,$(HOST_OS))
-  AND_CLANG_VERSION := $(shell prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(LLVM_PREBUILTS_VERSION)/bin/clang --version | grep "clang " 2>&1)
+  PROP_CLANG_VERSION := $(shell prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(LLVM_PREBUILTS_VERSION)/bin/clang --version | grep "clang " 2>&1)
 
   space :=
   space +=
-  AND_CLANG_VERSION := $(subst $(space),-,$(strip $(AND_CLANG_VERSION)))
+  PROP_CLANG_VERSION := $(subst $(space),-,$(strip $(PROP_CLANG_VERSION)))
 
   ADDITIONAL_BUILD_PROPERTIES += \
-    ro.build.clang=$(AND_CLANG_VERSION)
-  $(info Clang version for build.prop: '$(AND_CLANG_VERSION)')
+    ro.build.clang=$(PROP_CLANG_VERSION)
+
+  $(info Clang version for build.prop: '$(PROP_CLANG_VERSION)')
 
   ifeq (arm,$(TARGET_ARCH))
 
     # ANDROIDEABI TOOLCHAIN INFO
-    AND_TC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(TARGET_GCC_VERSION)
-    AND_TC_VERSION := $(shell $(AND_TC_PATH)/bin/arm-linux-androideabi-gcc --version 2>&1)
-    AND_TC_VERSION_NUMBER := $(shell $(AND_TC_PATH)/bin/arm-linux-androideabi-gcc -dumpversion 2>&1)
-    AND_TC_DATE := $(filter 20150% 20151% 20160% 20161% 20170% 20171%,$(AND_TC_VERSION))
+    PROP_GCC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(TARGET_GCC_VERSION)
+    PROP_GCC_VERSION := $(shell $(PROP_GCC_PATH)/bin/arm-linux-androideabi-gcc --version 2>&1)
+    PROP_GCC_VERSION_NUMBER := $(shell $(PROP_GCC_PATH)/bin/arm-linux-androideabi-gcc -dumpversion 2>&1)
+    PROP_GCC_DATE := $(filter 20150% 20151% 20160% 20161% 20170% 20171%,$(PROP_GCC_VERSION))
 
-    ifneq ($(filter (UBERTC%),$(AND_TC_VERSION)),)
-      AND_TC_NAME := UBERTC
+    ifneq ($(filter (UBERTC%),$(PROP_GCC_VERSION)),)
+      PROP_GCC_NAME := UBERTC
     else
-      AND_TC_NAME := GCC
+      PROP_GCC_NAME := GCC
     endif
 
-    ifeq (,$(AND_TC_DATE))
-      ARM_AND_PROP := $(AND_TC_NAME)-$(AND_TC_VERSION_NUMBER)
-    else
-      ARM_AND_PROP := $(AND_TC_NAME)-$(AND_TC_VERSION_NUMBER)-$(AND_TC_DATE)
-    endif
-
-    ADDITIONAL_BUILD_PROPERTIES += \
-      ro.build.gcc=$(ARM_AND_PROP)
-    $(info GCC version for build.prop: '$(ARM_AND_PROP)')
-
-    # ARM-EABI TOOLCHAIN INFO
-    ifneq ($(TARGET_GCC_VERSION_ARM),)
-      KERNEL_TC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-$(TARGET_GCC_VERSION_ARM)
-    else
-      KERNEL_TC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-$(TARGET_GCC_VERSION)
-    endif
-
-    KERNEL_TC_VERSION := $(shell $(KERNEL_TC_PATH)/bin/arm-eabi-gcc --version 2>&1)
-    KERNEL_TC_VERSION_NUMBER := $(shell $(KERNEL_TC_PATH)/bin/arm-eabi-gcc -dumpversion 2>&1)
-    KERNEL_TC_DATE := $(filter 20150% 20151% 20160% 20161% 20170% 20171%,$(KERNEL_TC_VERSION))
-
-    ifneq ($(filter (UBERTC%),$(KERNEL_TC_VERSION)),)
-      KERNEL_TC_NAME := UBERTC
-    else
-      KERNEL_TC_NAME := GCC
-    endif
-
-    ifeq (,$(KERNEL_TC_DATE))
-      ARM_KERNEL_PROP := $(KERNEL_TC_NAME)-$(KERNEL_TC_VERSION_NUMBER)
-    else
-      ARM_KERNEL_PROP := $(KERNEL_TC_NAME)-$(KERNEL_TC_VERSION_NUMBER)-$(KERNEL_TC_DATE)
+    ifeq (,$(PROP_GCC_DATE))
+      PROP_GCC_TEXT := $(PROP_GCC_NAME)-$(PROP_GCC_VERSION_NUMBER)
+    else	
+      PROP_GCC_TEXT := $(PROP_GCC_NAME)-$(PROP_GCC_VERSION_NUMBER)-$(PROP_GCC_DATE)
     endif
 
     ADDITIONAL_BUILD_PROPERTIES += \
-      ro.build.kernel=$(ARM_KERNEL_PROP)
-    $(info GCC version for build.prop: '$(ARM_KERNEL_PROP)')
+      ro.build.gcc=$(PROP_GCC_TEXT)
 
   else ifeq (arm64,$(TARGET_ARCH))
 
     # AARCH64 TOOLCHAIN INFO
     ifneq ($(TARGET_GCC_VERSION_ARM64),)
-      UBER_TC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-$(TARGET_GCC_VERSION_ARM64)
+      PROP_GCC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-$(TARGET_GCC_VERSION_ARM64)
     else
-      UBER_TC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9
+      PROP_GCC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9
     endif
 
-    UBER_TC_VERSION := $(shell $(UBER_TC_PATH)/bin/aarch64-linux-android-gcc --version)
-    UBER_TC_VERSION_NUMBER := $(shell $(UBER_TC_PATH)/bin/aarch64-linux-android-gcc -dumpversion 2>&1)
-    UBER_TC_DATE := $(filter 20150% 20151% 20160% 20161%,$(UBER_TC_VERSION))
-    ifneq ($(filter (UBERTC%),$(UBER_TC_VERSION)),)
-      UBER_TC_NAME := UBERTC
+    PROP_GCC_VERSION := $(shell $(PROP_GCC_PATH)/bin/aarch64-linux-android-gcc --version)
+    PROP_GCC_VERSION_NUMBER := $(shell $(PROP_GCC_PATH)/bin/aarch64-linux-android-gcc -dumpversion 2>&1)
+    PROP_GCC_DATE := $(filter 20150% 20151% 20160% 20161%,$(PROP_GCC_VERSION))
+    ifneq ($(filter (UBERTC%),$(PROP_GCC_VERSION)),)
+      PROP_GCC_NAME := UBERTC
     else
-      UBER_TC_NAME := GCC
+      PROP_GCC_NAME := GCC
     endif
 
-    ifeq (,$(UBER_TC_DATE))
-      AARCH64_AND_PROP := $(UBER_TC_NAME)-$(UBER_TC_VERSION_NUMBER)
+    ifeq (,$(PROP_GCC_DATE))
+      PROP_GCC_TEXT := $(PROP_GCC_NAME)-$(PROP_GCC_VERSION_NUMBER)
     else
-      AARCH64_AND_PROP := $(UBER_TC_NAME)-$(UBER_TC_VERSION_NUMBER)-$(UBER_TC_DATE)
+      PROP_GCC_TEXT := $(PROP_GCC_NAME)-$(PROP_GCC_VERSION_NUMBER)-$(PROP_GCC_DATE)
     endif
 
     ADDITIONAL_BUILD_PROPERTIES += \
-      ro.build.gcc=$(AARCH64_AND_PROP)
-    $(info GCC version for build.prop: '$(AARCH64_AND_PROP)')
+      ro.build.gcc=$(PROP_GCC_TEXT)
   endif
+
+  $(info GCC version for build.prop: '$(PROP_GCC_TEXT)')
+
 endif
