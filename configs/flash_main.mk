1ee5396a9eaaccbbdb10fd0316a95d620fdced2a
beanstown106 <nbenis106@gmail.com>
2016-12-17 03:11:42 -0500

    Add Pixel wallpapers
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- configs/flash_main.mk
+++ configs/flash_main.mk
@@ -40,7 +40,29 @@ PRODUCT_PACKAGES += \
 
 # Prebuilt applications
 PRODUCT_PACKAGES += \
-    PixelLauncherPrebuilt
+    PixelLauncherPrebuilt \
+    LiveWallpapersPicker \
+    NexusWallpapersStubPrebuilt \
+    WallpaperPickerGooglePrebuilt \
+    NexusLauncherPrebuilt \
+    WallpapersUsTwo
+
+# wallpaper location prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.wallpapers_loc_request_suw=true
+
+# live wallpapers arm64 only
+ifneq ($(filter angler bullhead marlin sailfish,$(TARGET_DEVICE)),)
+    PRODUCT_PACKAGES += \
+        WallpapersBReel \
+        libgdx.so \
+        libgeswallpapers-jni.so \
+        libjpeg.so
+
+    PRODUCT_COPY_FILES += \
+        vendor/flash/prebuilt/lib64/libgdx.so:system/lib64/libgdx.so \
+        vendor/flash/prebuilt/lib64/libgeswallpapers-jni.so:system/lib64/libgeswallpapers-jni.so
+endif
 
 # DU Utils Library
 PRODUCT_BOOT_JARS += \
