32d470248020195392b67009f06bc4d2806bd4d3
Nathan Chancellor <natechancellor@gmail.com>
2017-02-12 03:12:26 -0700

    Configs: TC versioning: Clean up and improve
    1. Make spacing look better for a makefile
    2. Apply GCC date logic to Clang, as people may not be building with my toolchains, which have a specific commit format that is the basis for the Clang date
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- configs/tcversion.mk
+++ configs/tcversion.mk
@@ -3,60 +3,65 @@
 
 UNAME := $(shell uname -s)
 ifeq (Linux,$(UNAME))
-  HOST_OS := linux
+    HOST_OS := linux
 endif
 
 ifeq (linux,$(HOST_OS))
-  PROP_CLANG_VERSION := $(shell prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(LLVM_PREBUILTS_VERSION)/bin/clang --version | grep "clang " | cut -d ' ' -f 3 2>&1)
-  PROP_CLANG_DATE := $(shell git -C prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(LLVM_PREBUILTS_VERSION) log -1 --format="%s" | cut -d ' ' -f 3 2>&1)
+    PROP_CLANG_VERSION := $(shell prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(LLVM_PREBUILTS_VERSION)/bin/clang --version | grep "clang " | cut -d ' ' -f 3 2>&1)
+    PROP_CLANG_DATE := $(shell git -C prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(LLVM_PREBUILTS_VERSION) log -1 --format="%s" | cut -d ' ' -f 3 2>&1)
+    PROP_CLANG_DATE := $(filter 20150% 20151% 20160% 20161% 20170% 20171% 20180% 20181%,$(PROP_CLANG_DATE))
 
-  PROP_CLANG_TEXT := $(PROP_CLANG_VERSION)-$(PROP_CLANG_DATE)
-
-  ADDITIONAL_BUILD_PROPERTIES += \
-    ro.build.clang=$(PROP_CLANG_TEXT)
-
-  $(info Clang version for build.prop: '$(PROP_CLANG_TEXT)')
-
-  ifeq (arm,$(TARGET_ARCH))
-
-    # ANDROIDEABI TOOLCHAIN INFO
-    PROP_GCC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(TARGET_GCC_VERSION)
-    PROP_GCC_VERSION := $(shell $(PROP_GCC_PATH)/bin/arm-linux-androideabi-gcc --version 2>&1)
-    PROP_GCC_VERSION_NUMBER := $(shell $(PROP_GCC_PATH)/bin/arm-linux-androideabi-gcc -dumpversion 2>&1)
-    PROP_GCC_DATE := $(filter 20150% 20151% 20160% 20161% 20170% 20171% 20180% 20181%,,$(PROP_GCC_VERSION))
-
-    ifeq (,$(PROP_GCC_DATE))
-      PROP_GCC_TEXT := $(PROP_GCC_VERSION_NUMBER)
-    else	
-      PROP_GCC_TEXT := $(PROP_GCC_VERSION_NUMBER)-$(PROP_GCC_DATE)
+    ifneq ($(PROP_CLANG_DATE),)
+        PROP_CLANG_TEXT := $(PROP_CLANG_VERSION)-$(PROP_CLANG_DATE)
+    else
+        PROP_CLANG_TEXT := $(PROP_CLANG_VERSION)
     endif
 
     ADDITIONAL_BUILD_PROPERTIES += \
-      ro.build.gcc=$(PROP_GCC_TEXT)
+        ro.build.clang=$(PROP_CLANG_TEXT)
 
-  else ifeq (arm64,$(TARGET_ARCH))
+    $(info Clang version for build.prop: '$(PROP_CLANG_TEXT)')
 
-    # AARCH64 TOOLCHAIN INFO
-    ifneq ($(TARGET_GCC_VERSION_ARM64),)
-      PROP_GCC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-$(TARGET_GCC_VERSION_ARM64)
-    else
-      PROP_GCC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9
+    ifeq (arm,$(TARGET_ARCH))
+
+        # ANDROIDEABI TOOLCHAIN INFO
+        PROP_GCC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(TARGET_GCC_VERSION)
+        PROP_GCC_VERSION := $(shell $(PROP_GCC_PATH)/bin/arm-linux-androideabi-gcc --version 2>&1)
+        PROP_GCC_VERSION_NUMBER := $(shell $(PROP_GCC_PATH)/bin/arm-linux-androideabi-gcc -dumpversion 2>&1)
+        PROP_GCC_DATE := $(filter 20150% 20151% 20160% 20161% 20170% 20171% 20180% 20181%,,$(PROP_GCC_VERSION))
+
+        ifeq (,$(PROP_GCC_DATE))
+            PROP_GCC_TEXT := $(PROP_GCC_VERSION_NUMBER)
+        else
+            PROP_GCC_TEXT := $(PROP_GCC_VERSION_NUMBER)-$(PROP_GCC_DATE)
+        endif
+
+        ADDITIONAL_BUILD_PROPERTIES += \
+            ro.build.gcc=$(PROP_GCC_TEXT)
+
+    else ifeq (arm64,$(TARGET_ARCH))
+
+        # AARCH64 TOOLCHAIN INFO
+        ifneq ($(TARGET_GCC_VERSION_ARM64),)
+            PROP_GCC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-$(TARGET_GCC_VERSION_ARM64)
+        else
+            PROP_GCC_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9
+        endif
+
+        PROP_GCC_VERSION := $(shell $(PROP_GCC_PATH)/bin/aarch64-linux-android-gcc --version)
+        PROP_GCC_VERSION_NUMBER := $(shell $(PROP_GCC_PATH)/bin/aarch64-linux-android-gcc -dumpversion 2>&1)
+        PROP_GCC_DATE := $(filter 20150% 20151% 20160% 20161% 20170% 20171% 20180% 20181%,,$(PROP_GCC_VERSION))
+
+        ifeq (,$(PROP_GCC_DATE))
+            PROP_GCC_TEXT := $(PROP_GCC_VERSION_NUMBER)
+        else
+            PROP_GCC_TEXT := $(PROP_GCC_VERSION_NUMBER)-$(PROP_GCC_DATE)
+        endif
+
+        ADDITIONAL_BUILD_PROPERTIES += \
+            ro.build.gcc=$(PROP_GCC_TEXT)
     endif
 
-    PROP_GCC_VERSION := $(shell $(PROP_GCC_PATH)/bin/aarch64-linux-android-gcc --version)
-    PROP_GCC_VERSION_NUMBER := $(shell $(PROP_GCC_PATH)/bin/aarch64-linux-android-gcc -dumpversion 2>&1)
-    PROP_GCC_DATE := $(filter 20150% 20151% 20160% 20161% 20170% 20171% 20180% 20181%,,$(PROP_GCC_VERSION))
-
-    ifeq (,$(PROP_GCC_DATE))
-      PROP_GCC_TEXT := $(PROP_GCC_VERSION_NUMBER)
-    else
-      PROP_GCC_TEXT := $(PROP_GCC_VERSION_NUMBER)-$(PROP_GCC_DATE)
-    endif
-
-    ADDITIONAL_BUILD_PROPERTIES += \
-      ro.build.gcc=$(PROP_GCC_TEXT)
-  endif
-
-  $(info GCC version for build.prop: '$(PROP_GCC_TEXT)')
+    $(info GCC version for build.prop: '$(PROP_GCC_TEXT)')
 
 endif
