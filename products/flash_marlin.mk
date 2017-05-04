ea6946da0f5630abe8e7ecd6f75a30d2d2b6fe5a
f100cleveland <dustin.rinne@gmail.com>
2017-05-03 21:34:17 -0500

    Initial pissel flash

---
+++ products/flash_marlin.mk
@@ -0,0 +1,52 @@
+# Copyright (C) 2017 Flash ROM
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include custom telephony configuration
+include vendor/flash/configs/flash_phone.mk
+
+# Copy over our ramdisk files
+PRODUCT_COPY_FILES += \
+    vendor/flash/prebuilt/ramdisk/init.flash.rc:root/init.flash.rc \
+    vendor/flash/prebuilt/ramdisk/init.profiles.rc:root/init.profiles.rc \
+    vendor/flash/prebuilt/ramdisk/init.profiles.sh:root/init.profiles.sh \
+    vendor/flash/prebuilt/ramdisk/init.special_power.sh:root/init.special_power.sh \
+    vendor/flash/prebuilt/ramdisk/msm_irqbalance.conf:root/msm_irqbalance.conf
+
+# Include Spectrum APK
+PRODUCT_PACKAGES += \
+    Spectrum
+
+# Inherit AOSP device configuration for angler
+$(call inherit-product, device/google/marlin/aosp_marlin.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := flash_marlin
+PRODUCT_DEVICE := marlin
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel XL
+PRODUCT_MANUFACTURER := Google
+
+#Inline kernel
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-6.3/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_CONFIG := fuckery_defconfig
+TARGET_KERNEL_SOURCE := kernel/google/pixel
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=marlin \
+    BUILD_FINGERPRINT=google/marlin/marlin:7.1.2/N2G47J/3831342:user/release-keys \
+    PRIVATE_BUILD_DESC="marlin-user 7.1.2 N2G47J 3831342 release-keys"
